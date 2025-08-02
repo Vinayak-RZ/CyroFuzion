9a8b93983725052c744ffbbc6cf4a79851f6490a
N4647 <nitinyadav4637@gmail.com>
2025-08-02 15:19:45 +0530

    feat: added auction price graph using mock data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUpPage from "./components/Auth/SignUp"; 
 import LoginPage from "./components/Auth/Login";
 import Home from "./components/Home/Home";
+import AuctionPage from "./components/Auction/price";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/register" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/auction" element={<AuctionPage auctionId={1} />} />
       </Routes>
     </Router>
   );
